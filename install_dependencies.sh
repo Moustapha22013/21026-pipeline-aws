906ab62e4219f69e0e7a5e8333e770631d2c4681
mustapha2200 <22013@supnum.mr>
2026-06-19 02:53:16 +0000

    fix: remove WAR in BeforeInstall and ApplicationStop

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 yum install tomcat10 -y || dnf install tomcat10 -y
+sudo rm -f /usr/share/tomcat10/webapps/lab2-webapp.war || true
+sudo rm -f /var/lib/tomcat10/webapps/lab2-webapp.war || true
